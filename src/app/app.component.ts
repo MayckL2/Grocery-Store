4efe3d6c1297427be9cf56b6e6c0005077b9746f
mayckl2 <mayckluciano2@gmail.com>
2023-12-26 23:27:35 -0300

    Feat: stock and rendering sneakers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import stock from './stock.json'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'store-angular';
+  tenis: string = ''
+  img: string = ''
+  stock: any = stock
+
+  constructor(){
+    console.log(stock)
+    this.tenis = stock[0].modelo
+    this.img = stock[0].img
+  }
+
+  select(choice: any){
+    console.log(choice)
+  }
 }
