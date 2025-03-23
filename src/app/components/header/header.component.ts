348278824c61454bd2109b43b33ea5b77e2322d5
mayckl2 <mayckluciano2@gmail.com>
2025-03-22 23:32:34 -0300

    ✨ feat: Header pt2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [InputTextModule, CommonModule, FormsModule, FloatLabel],
+  imports: [InputTextModule, CommonModule, FormsModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
