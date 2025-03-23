9429d0aa2dbdbb55ed0874bbfad10293ae37b821
mayckl2 <mayckluciano2@gmail.com>
2025-03-23 00:45:07 -0300

    ✨ feat: Comp. Category

---
+++ src/app/components/header/category/category/category.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, input } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
+export interface Options {
+  name: string,
+  destination: string | undefined
+}
+
+@Component({
+  selector: 'app-category',
+  imports: [SelectModule, FormsModule, MatButtonModule, MatMenuModule, CommonModule],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.scss'
+})
+export class CategoryComponent {
+  @Input() name: string = '';
+  @Input() options: Options[] | undefined;
+  // options: any = input<Options[]>();
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,14 +4,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { MatIconModule } from '@angular/material/icon';
+import { CategoryComponent, Options } from "./category/category/category.component";
 
 @Component({
   selector: 'app-header',
-  imports: [InputTextModule, CommonModule, FormsModule, MatIconModule],
+  imports: [InputTextModule, CommonModule, FormsModule, MatIconModule, CategoryComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   imageLogo = '/assets/FreshcartLogo.png'
   value = ''
+  array: Options[] | undefined = [
+    {name: "tomato", destination: "sei la porra"},
+    {name: "tomato", destination: "sei la porra"},
+    {name: "tomato", destination: "sei la porra"}
+  ]
 }
