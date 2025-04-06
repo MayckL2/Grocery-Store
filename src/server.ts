a32702ff0b709f5cee1d44a878ff5681dbcbb246
Mayck <Mayckluciano2@gmail.com>
2025-04-06 20:38:45 -0300

    Parameters on server side fix pt.7

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import { fileURLToPath } from 'node:url';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 import { join } from 'path';
+import { AppServerModule } from './main.server';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
