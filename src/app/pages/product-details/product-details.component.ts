d08b98548d604af9b6f0f6f6253928a69524c3f6
mayckl2 <mayckluciano2@gmail.com>
2025-04-07 12:45:29 -0300

    ✨ feat: Product details pt 1
    Product detail funcionalities and styling

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from '../../models/IProduct';
 
 @Component({
   selector: 'app-product-details',
@@ -6,6 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
+  service = inject(ApiService);
+  route = inject(ActivatedRoute);
+  productData: IProduct | undefined;
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    // console.log(this.service.getProduct(Number(id)));  
+    this.productData = this.service.getProduct(Number(id))
+  }
 }
