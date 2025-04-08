466077ffd4ff34d786fe117725292d5bb187e38f
mayckl2 <mayckluciano2@gmail.com>
2025-04-08 16:11:18 -0300

    ✨ feat: detail funcionality
    Product detail funcionality, quantity, stock and show values

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
 import { IProduct } from '../../models/IProduct';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-product-details',
-  imports: [],
+  imports: [RouterLink, RouterModule, ButtonModule, CommonModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
@@ -13,10 +16,27 @@ export class ProductDetailsComponent implements OnInit{
   service = inject(ApiService);
   route = inject(ActivatedRoute);
   productData: IProduct | undefined;
+  quantity: number = 0
+  delivery: number = 0
+
+  addQuantity(){
+    if(this.quantity < this.productData!.inStock){
+      this.quantity ++
+    }
+  }
+
+  removeQuantity(){
+    if(this.quantity > 0){
+      this.quantity --
+    }
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
-    // console.log(this.service.getProduct(Number(id)));  
     this.productData = this.service.getProduct(Number(id))
+    console.log(this.productData);  
+
+    // DEFINING DELIVERY TIME
+    this.delivery = 2
   }
 }
