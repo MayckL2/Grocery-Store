f983d76d31155c01b7fd467bfadc4b1853e15e49
mayckl2 <mayckluciano2@gmail.com>
2025-04-16 04:47:25 -0300

    ✨ feat: Cart components

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api/api.service';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { ICompra } from '../../models/compra';
+import { Tag } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { CartItemComponent } from "../../components/cart-item/cart-item.component";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [RouterLink, RouterModule, ButtonModule, CommonModule, CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
+  service = inject(ApiService);
+  route = inject(ActivatedRoute);
+  products: ICompra[] | undefined;
+  
 
+  ngOnInit(): void {
+    this.products = this.service.getCart();
+    console.log(this.products);  
+  }
 }
