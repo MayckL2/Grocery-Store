312eacecc1f2e252a8329d0f4a326bd835665768
mayckl2 <mayckluciano2@gmail.com>
2025-04-18 04:12:25 -0300

    ✨ feat: product service pt.2
    getcategories
    quantity when calculate price

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -4,11 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductService {
+  protected categories: string[] = [
 
+  ]
   constructor() { }
 
-  calculatePrice(price: number, discount: number = 0, quantity: number = 0){
-    let finalValue = price - ( price * ( discount / 100)) * quantity;
+  calculatePrice(price: number, discount: number = 0, quantity: number = 1){
+    let finalValue = (price - ( price * ( discount / 100))) * quantity;
     return finalValue;
+  }
+
+  getCategories(){
+    this.categories = [
+      "Fruits & Vegetables",
+      "Dairy & Breakfast",
+      "Egg, Meat & Fish",
+      "Bath & Body",
+      "Cold drinks & Juices",
+      "Snacks and Munchies",
+      "Ice Delights"
+    ]
+    return this.categories;
   }
 }
