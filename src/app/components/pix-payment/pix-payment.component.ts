70ab1147502e0a46ce991a624521d0b4979d6990
mayckl2 <mayckluciano2@gmail.com>
2025-04-24 03:44:23 -0300

    ✨ feat: Fake pix

---
+++ src/app/components/pix-payment/pix-payment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pix-payment',
+  imports: [],
+  templateUrl: './pix-payment.component.html',
+  styleUrl: './pix-payment.component.scss'
+})
+export class PixPaymentComponent {
+
+}
