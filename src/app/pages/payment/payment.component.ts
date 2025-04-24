b9700c908bf66a8106ba9c19545ec237f2217851
mayckl2 <mayckluciano2@gmail.com>
2025-04-24 03:51:47 -0300

    ✨ feat: Payment feature
    Payment method
    confirm products and payments
    payment

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,21 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnChanges, SimpleChanges } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
 import { IProduct } from '../../models/IProduct';
 import { ActivatedRoute } from '@angular/router';
 import { StepperModule } from 'primeng/stepper';
 import { ButtonModule } from 'primeng/button';
-import { Location } from '@angular/common';
-import { FormAdressComponent } from "../../components/form-adress/form-adress.component";
+import { CommonModule, Location, ViewportScroller } from '@angular/common';
+import { FormAdressComponent } from '../../components/form-adress/form-adress.component';
 import { IAdress } from '../../models/IAdress';
 import { PaymentService } from '../../services/payment/payment.service';
+import { PaymentMethodComponent } from '../../components/payment-method/payment-method.component';
+import { FormConfirmationComponent } from "../../components/form-confirmation/form-confirmation.component";
+import { PixPaymentComponent } from "../../components/pix-payment/pix-payment.component";
+import { CardPaymentComponent } from "../../components/card-payment/card-payment.component";
+import { BankSlipPaymentComponent } from "../../components/bank-slip-payment/bank-slip-payment.component";
 
 @Component({
   selector: 'app-payment',
-  imports: [ButtonModule, StepperModule, FormAdressComponent],
+  imports: [
+    ButtonModule,
+    StepperModule,
+    FormAdressComponent,
+    PaymentMethodComponent,
+    FormConfirmationComponent,
+    CommonModule,
+    PixPaymentComponent,
+    CardPaymentComponent,
+    BankSlipPaymentComponent
+],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.scss',
 })
-export class PaymentComponent {
+export class PaymentComponent{
   service = inject(ApiService);
   route = inject(ActivatedRoute);
   payment = inject(PaymentService);
@@ -24,18 +39,37 @@ export class PaymentComponent {
     one: true,
     two: true,
     three: true,
-    four: true
+    four: true,
   };
+  step: number = 1
+  viewPort = inject(ViewportScroller);
+  method: string = 'pix';
 
-  constructor(private location: Location) { }
+  constructor(private location: Location) {}
 
-  back(){
+  nextStep(){
+    this.step ++
+    this.viewPort.scrollToPosition([0, this.step * 20]);
+
+    if(this.step == 4) this.renderPayment();
+  }
+
+  previousStep(){
+    this.step --
+    this.viewPort.scrollToPosition([0, 0]);
+  }
+
+  renderPayment(){
+    this.method = this.payment.getPaymentMethod();
+    console.log(this.method);
+  }
+
+  back() {
     this.location.back();
   }
 
-  colectAdress(adress: IAdress){
-    console.log(this.payment.validateAdress(adress));
-    if(this.payment.validateAdress(adress) == true){
+  colectAdress(adress: IAdress) {
+    if (this.payment.validateAdress(adress) == true) {
       this.disableNext.one = false;
       this.payment.saveAdress(adress);
     }
@@ -44,6 +78,5 @@ export class PaymentComponent {
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.productData = this.service.getProduct(Number(id));
-    console.log(this.productData);
   }
 }
--- src/app/services/payment/payment.service.ts
+++ src/app/services/payment/payment.service.ts
@@ -1,46 +1,116 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { IAdress } from '../../models/IAdress';
+import { Subject } from 'rxjs';
+import { IPayment } from '../../models/ipayment';
+import { ProductService } from '../product/product.service';
+import { IProduct } from '../../models/IProduct';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentService {
-  protected dataAdress: IAdress | undefined
+  protected dataAdress: IAdress | undefined;
+  protected paymentMethod: IPayment | undefined;
+  protected products: IProduct[] = [];
+  productService = inject(ProductService);
+  api = inject(ApiService);
 
-  constructor() { }
+  constructor() {}
 
-  saveAdress(adress: IAdress){
-    this.dataAdress = adress
-    return "Adress save!"
+  saveProduct(product: IProduct) {
+    this.products.push(product);
+    console.log(product);
   }
 
-  getAdress(){
-    return this.dataAdress
+  getProduct(): IProduct[] | undefined {
+    return this.products ? this.products : undefined;
   }
 
-  validateAdress(adress: IAdress): string|boolean{
-    if(!adress.cep){
-      return "CEP is necessary...";
+  saveAdress(adress: IAdress) {
+    this.dataAdress = adress;
+    return 'Adress save!';
+  }
+
+  getAdress() {
+    return this.dataAdress;
+  }
+
+  validateAdress(adress: IAdress): string | boolean {
+    if (!adress.cep) {
+      return 'CEP is necessary...';
     }
-    if(!adress.state){
-      return "State is necessary...";
+    if (!adress.state) {
+      return 'State is necessary...';
     }
-    if(!adress.neighborhood){
-      return "Neighborhood is necessary...";
+    if (!adress.neighborhood) {
+      return 'Neighborhood is necessary...';
     }
-    if(!adress.street){
-      return "Street is necessary...";
+    if (!adress.street) {
+      return 'Street is necessary...';
     }
-    if(!adress.number){
-      return "Number is necessary...";
+    if (!adress.number) {
+      return 'Number is necessary...';
     }
-    if(!adress.complement){
-      return "Complement is necessary...";
+    if (!adress.complement) {
+      return 'Complement is necessary...';
     }
-    if(!adress.receiver){
-      return "Receiver is necessary...";
+    if (!adress.receiver) {
+      return 'Receiver is necessary...';
     }
- 
-    return true
+
+    return true;
+  }
+
+  savePaymentMethod(method: IPayment) {
+    this.paymentMethod = method;
+    console.log(this.paymentMethod)
+  }
+
+  getPaymentMethod(): IPayment | string {
+    let method: IPayment = this.paymentMethod!;
+    switch (method) {
+      case IPayment.pix:
+        return 'pix'
+        break;
+      case IPayment.debit:
+        return 'debit'
+        break;
+      case IPayment.credit:
+        return 'credit'
+        break;
+      case IPayment.voucher:
+        return 'voucher'
+        break;
+      case IPayment.bankSlip:
+        return 'bankSlip'
+        break;
+      default:
+        return 'qualquer bosta'  
+        break;
+    }
+  }
+
+  calcTotal(products: IProduct[]){
+    let items = (this.calcProducts(products) + this.calcDelivery(products)) - this.calcDiscount(products);
+    return items;
+  }
+
+  calcProducts(products: IProduct[]){
+    let items = 0;
+    products.map(e => items += e.price);
+    return items;
+  }
+
+  calcDiscount(products: IProduct[]){
+    let items = 0;
+    products.map(e => items += e.price * ( e.discount / 100) * (e.quantity ?? 1));
+    return items;
+  }
+
+  calcDelivery(products: IProduct[]){
+    let items = 0;
+    products?.map(e => items += 1); // to implement
+    return items;
   }
 }
