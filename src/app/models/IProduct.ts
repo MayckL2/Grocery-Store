065bbdc5e89fa263a2d9271e7d2fbd6378d0041d
mayckl2 <mayckluciano2@gmail.com>
2025-04-24 03:49:59 -0300

    🦄 refactor: Adjusting models

--- src/app/models/IProduct.ts
+++ src/app/models/IProduct.ts
@@ -2,16 +2,18 @@ export interface IProduct {
     id: number,
     name: string,
     price: number,
-    discount?: number,
+    discount: number,
     image: string,
     inStock: number,
     category: string,
+    quantity?: number,
 }
 
 export const productDefault: IProduct = {
     id: 0,
     name: "",
     price: 0,
+    discount: 0,
     image: "",
     inStock: 0,
     category: ""
---
+++ src/app/models/ipayment.ts
@@ -0,0 +1,7 @@
+export enum IPayment {
+    'pix'= 'pix',
+    'debit'= 'debit',
+    'credit'= 'credit',
+    'voucher'= 'voucher',
+    'bankSlip'= 'bankSlip'
+}
