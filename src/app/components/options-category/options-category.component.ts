e33635146cfdccb9723f704f99577747c33e7810
mayckl2 <mayckluciano2@gmail.com>
2025-04-26 01:49:00 -0300

    ✨ feat: options categories

--- src/app/components/options-category/options-category.component.ts
+++ src/app/components/options-category/options-category.component.ts
@@ -1,18 +1,53 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ApiService } from '../../services/api/api.service';
 import { ICategory } from '../../models/ICategory';
+import { isPlatformBrowser } from '@angular/common';
+import { Swiper } from 'swiper';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
-  selector: 'app-options-category',
+  selector: 'div[app-options-category]',
   imports: [],
   templateUrl: './options-category.component.html',
-  styleUrl: './options-category.component.scss'
+  styleUrl: './options-category.component.scss',
+  host: {
+    class: 'md:px-20 px-8 py-6'
+  }
 })
 export class OptionsCategoryComponent implements OnInit {
   api = inject(ApiService);
   options: ICategory[] = [];
 
-  ngOnInit(): void {
+  private config: SwiperOptions = {
+    breakpoints: {
+      555: {
+        slidesPerView: 2,
+      },
+      880: {
+        slidesPerView: 4,
+      },
+      1115: {
+        slidesPerView: 5,
+      },
+      1295: {
+        slidesPerView: 6,
+      }
+    },
+    spaceBetween: 10,
+    navigation: false,
+    pagination: { clickable: false },
+    scrollbar: { draggable: true },
+    // Outras opções de configuração
+  };
+
+  // No seu componente
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private el: ElementRef) {}
+
+  ngOnInit() {
     this.options = this.api.getCategories();
+    if (isPlatformBrowser(this.platformId)) {
+      // Inicialize o Swiper aqui
+      new Swiper(this.el.nativeElement.querySelector('.swiper-container'), this.config);
+    }
   }
 }
